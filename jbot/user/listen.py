c8e6592bf60c135dad51fa29ac362c21197a377e
chea0214 <chea0214@163.com>
2022-10-05 22:17:20 +0800

    更新

--- jbot/user/listen.py
+++ jbot/user/listen.py
@@ -5,7 +5,7 @@ import requests
 import re
 
 
-QL_URL = "http://172.17.0.3:5700"
+QL_URL = "http://172.17.0.5:5700"
 Client_Id = "4V-ZvI8Y_NK6"
 Client_Secret = "c4-KUDKG74Zq6Ux5Wm0fW1-2"
 Authorization = ""
@@ -35,31 +35,35 @@ async def subupdate(event):
 
 @user.on(events.NewMessage(pattern=r'^拉库日志$', outgoing=True))
 async def subupdate_log(event):
+    text = "开始提取日志..."
     res = requests.get(QL_URL + "/open/subscriptions", headers=headers).json()
     if res['code'] == 200:
         for env in res['data']:
             if env['name'] == KuName:
                 res = requests.get(QL_URL + "/open/subscriptions/1/log", headers=headers).json()
                 if res['code'] == 200:
-                    await event.edit(res['data'])
+                    text = res['data']
+                else:
+                    text = "日志提取失败"
+        else:
+            text = "未找到关键字仓库"
+    else:
+        text = "订阅获取失败"
+    await event.edit(res['data'])
 
 
 @user.on(events.NewMessage(incoming=True))
 async def mylisten(event):
     if event.is_channel:
         if event.chat_id == listen_CK:
-            await user.send_message(2028602503, "仓库更新")
             res = re.search(KeyWord, event.raw_text)
             if res:
+                await user.send_message(2028602503, str("更新仓库"))
                 LQCK()
-                text = "正则更新仓库"
-            else:
-                text = "未找到关键字"
-            await user.send_message(2028602503, str(text))
-#        elif event.chat_id == listen_CH:
-#            res = re.findall("LUCK_DRAW_URL=\"(\S*)\"", event.raw_text)
-#            if res:
-#                ZXRW("LUCK_DRAW_URL", res[0], "jd_luck_draw.js")
+#       elif event.chat_id == listen_CH:
+#           res = re.findall("LUCK_DRAW_URL=\"(\S*)\"", event.raw_text)
+#           if res:
+#               ZXRW("LUCK_DRAW_URL", res[0], "jd_luck_draw.js")
 
 
 def LQCK():
@@ -72,8 +76,8 @@ def LQCK():
                     text = "订阅更新开始..."
                 else:
                     text = "订阅更新未执行，请检查"
-            else:
-                text = "未找到拉库设置的名称"
+        else:
+            text = "未找到拉库设置的名称"
     else:
         text = "青龙登录失败，请检查青龙地址、Client_Id、Client_Secret"
     return text
