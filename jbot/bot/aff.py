02d2f61597a18de8a240746486adaa74b8f36fed
chea0214 <chea0214@163.com>
2022-10-06 01:03:30 +0800

    更新

--- jbot/bot/aff.py
+++ jbot/bot/aff.py
@@ -3,4 +3,4 @@ from .. import jdbot, chat_id,JD_DIR
 
 @jdbot.on(events.NewMessage(from_users=chat_id, pattern=r'^/aff$'))
 async def bot_aff(event):
-    await jdbot.send_message(chat_id,'感谢您')
+    await jdbot.send_message(chat_id,'感谢您!')
